3f8879c4d9bd4305c647e6c6bb97e7b3dddaeb57
leandjb <leandjb@gmail.com>
2020-06-12 14:09:26 -0500

    #5: update maptile gh-pages

--- js/map.js
+++ js/map.js
@@ -2,11 +2,11 @@
 
 /* servicios tilemap */
 //TileOSM Gray
-// const tileProvider = "https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png";
+const tileProvider = "https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png";
 
 //tileStadia
-const tileProvider =
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png";
+// const tileProvider =
+//   "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png";
 
 //tileStadia Outdoors
 // const tileProvider ="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png";
